74f4c9f4913862ce193f243c00c42a49f41e178f
ksc <ksc036@gmail.com>
2025-04-23 06:11:46 +0900

    url change

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -10,12 +10,13 @@ router.post("/", api("createPost"));
 // 2. 게시글 목록 조회
 router.get("/", api("getAllPosts"));
 
-// 3. 게시글 단건 조회
+// 3. 게시글 삭제
+router.delete("/", api("deletePost"));
+
+// 4. 게시글 단건 조회
 router.get("/:id", api("getPost"));
 
-// 4. 게시글 수정
+// 5. 게시글 수정
 router.put("/:id", api("updatePost"));
 
-// 5. 게시글 삭제
-router.delete("/:id", api("deletePost"));
 export default router;
